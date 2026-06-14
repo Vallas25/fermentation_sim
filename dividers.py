021fd5ba3a7e80366116e98a9910dc679013f27f
Vallas25 <jansensmaurits@gmail.com>
2026-06-14 12:17:25 +0200

    added fedbatch panel

--- dividers.py
+++ dividers.py
@@ -3,8 +3,42 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from START_VALUES import *
 
+fed_batch_panel = html.Div([
+    html.Label("Fedbatch?"),
+    dcc.Checklist(options=[
+        {'label': 'Fed-Batch Mode', 'value': 'fedbatch_on'}
+    ],
+    value=[],
+    id="fedbatch"),
+    html.Div([
+        html.Label("initial volume"),
+        dcc.Input(type="number", id="initial_volume"),
+    ],
+    style={
+        "display": "flex",
+        "flexDirection": "column",
+    }
+    ),
+    html.Label("feed rate (l/min)"),
+    dcc.Slider(
+        id="feed_rate",
+        min=0,
+        max=30,
+        step=1,
+        value=15
+    ),
+    html.Label("Substrate concentration (g/l)"),
+    dcc.Slider(
+        id="fedbatch_concentration",
+        min=0,
+        max=30,
+        step=1,
+        value=substrate_start
+    )
+])
+
 controls_panel = html.Div([
-            html.Label("Run time (min)"),
+        html.Label("Run time (min)"),
         dcc.Slider(id="run_time",
                    min=10,
                    max=1000,
@@ -24,6 +58,11 @@ controls_panel = html.Div([
                    max=40,
                    step=1,
                    value=substrate_start),
+        
+        html.Label("Max vollume"),
+        dcc.Input(type="number", id="max_value"),
+
+        fed_batch_panel,
 
         html.Button("Run simulation", id="run-btn"),
     ],
